07c8a25634a7a3cbfefd56b61444c394c627219a
Dhanya <144830256+DAurora03@users.noreply.github.com>
2025-09-14 15:23:59 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyNACNWMiiacdbiC1A/2JAZH",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
